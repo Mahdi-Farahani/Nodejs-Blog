c456bfda32c3b87d9b25989f7bd558a6b8535322
mahdi farahani <mahdifrhn91@gmail.com>
2021-05-26 16:52:56 +0430

    make it better

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -8,17 +7,23 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
-const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL =
+	'mongodb+srv://mahdi:Frhn0521369@cluster0.7097n.mongodb.net/ilaraweb?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+mongoose
+	.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() =>
+		app.listen(PORT, () =>
+			console.log(`Server Running on Port: http://localhost:${PORT}`)
+		)
+	)
+	.catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
