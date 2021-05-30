715f41c3d305b67e6570a11b74228e16551fefaf
mahdi farahani <mahdifrhn91@gmail.com>
2021-05-30 19:09:45 +0430

    change for heroku connections

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <Typography className={classes.heading} variant="h2" align="center">My App Mahdi</Typography>
         <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       <Grow in>
--- server/index.js
+++ server/index.js
@@ -13,8 +13,12 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res) => {
+	res.send('hello to ilaraweb API');
+});
+
 const CONNECTION_URL =
-	'mongodb+srv://mahdi:Frhn0521369@cluster0.7097n.mongodb.net/ilaraweb?retryWrites=true&w=majority';
+	'mongodb+srv://mahdi:Frhn0521369@cluster0.7097n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose
